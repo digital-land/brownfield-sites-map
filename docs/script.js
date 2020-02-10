8ac871aa17fc43dc5024cd578f0164cba2e1adfa
Paul Downey <psd@whatfettle.com>
2020-02-10 14:21:54 +0000

    Relative data link for testing locally

--- docs/script.js
+++ docs/script.js
@@ -25,7 +25,7 @@ function popup (data) {
   return datastring
 }
 
-Papa.parse('https://digital-land.github.io/map/data/brownfield.csv', {
+Papa.parse('data/brownfield.csv', {
   download: true,
   header: true,
   step: function (row) {
