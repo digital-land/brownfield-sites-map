cbe0eb7bf9ce409c044c0b56d07bd38a6825e2b9
Jake Mulley <jakemulley@users.noreply.github.com>
2020-03-17 09:45:42 +0000

    Remove commented code

--- docs/boundary-referral.js
+++ docs/boundary-referral.js
@@ -9,6 +9,18 @@ L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map)
 
+// Brownfield Features
+var brownfieldFeatures = brownfield.map(function (point) {
+  return {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [point.longitude, point.latitude]
+    },
+    properties: point
+  }
+})
+
 // Local authority boundaries (England only)
 geoJson = geoJson.features.filter(function (item) {
   return item.properties.lad19cd.startsWith('E')
@@ -20,17 +32,6 @@ geoJson = geoJson.features.filter(function (item) {
   })
 
   return item
-})
-
-var brownfieldFeatures = brownfield.map(function (point) {
-  return {
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [point.longitude, point.latitude]
-    },
-    properties: point
-  }
 })
 
 L.geoJSON(geoJson, {
@@ -68,33 +69,23 @@ L.geoJSON(geoJson, {
       var brownfieldOnMap = L.geoJSON({
         type: 'FeatureCollection',
         features: thisOrganisationsFeatures
+      }, {
+        pointToLayer: function (feature, latlng) {
+          var size = isNaN(feature.properties.hectares) ? 100 : (Math.sqrt((feature.properties.hectares * 10000) / Math.PI))
+          return L.circle(latlng, { color: 'red', fillColor: '#f03', fillOpacity: 0.5, radius: size.toFixed(2) })
+        },
+        onEachFeature: function (feature, layer) {
+          layer.on({
+            click: function () {
+              console.log(feature, layer)
+            }
+          })
+        }
       })
 
       brownfieldMarkers.addLayer(brownfieldOnMap)
       map.addLayer(brownfieldMarkers)
     }
-
-    //     mouseover: function () {
-    //       layer.openPopup()
-    //       this.setStyle({
-    //         fillColor: 'black'
-    //       })
-    //     },
-    //     mouseout: function () {
-    //       layer.closePopup()
-    //       if (!html) {
-    //         this.setStyle({
-    //           fillColor: 'red'
-    //         })
-    //       } else {
-    //         this.setStyle({
-    //           fillColor: 'white'
-    //         })
-    //       }
-    //     },
-    //     click: function () {
-    //       return window.location.assign('https://digital-land.github.io/resource/' + html.data.resource + '/#map')
-    //     }
 
     layer.on({
       mouseover: function () {
@@ -113,149 +104,5 @@ L.geoJSON(geoJson, {
         return map.fitBounds(layer.getBounds())
       }
     })
-
-    // var brownfieldMarkers = L.markerClusterGroup({ chunkedLoading: true })
-    // var brownfieldOnMap = L.geoJSON(brownfieldGeoJson)
-    // brownfieldMarkers.addLayer(brownfieldOnMap)
-    // map.addLayer(brownfieldMarkers)
-    // map.fitBounds(markers.getBounds())
-
-    // return layer.bindTooltip(count, {
-    //   direction: 'top',
-    //   permanent: true
-    // }).openTooltip()
   }
 }).addTo(map)
-
-// Brownfield points
-// var brownfieldGeoJson = {
-//   type: 'FeatureCollection',
-//   features: brownfield.map(function (point) {
-//     return {
-//       type: 'Feature',
-//       geometry: {
-//         type: 'Point',
-//         coordinates: [point.longitude, point.latitude]
-//       },
-//       properties: point
-//     }
-//   })
-// }
-
-// var brownfieldMarkers = L.markerClusterGroup({ chunkedLoading: true })
-// var brownfieldOnMap = L.geoJSON(brownfieldGeoJson)
-// brownfieldMarkers.addLayer(brownfieldOnMap)
-// map.addLayer(brownfieldMarkers)
-// map.fitBounds(markers.getBounds())
-
-// console.log(brownfieldGeoJson)
-
-// L.geoJSON(brownfieldGeoJson).addTo(map)
-
-// // Initial map view
-// var boundaries = {
-//   'Local authorities': L.layerGroup()
-// }
-
-// const base = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-//   id: 'base',
-//   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// })
-
-// const map = L.map('map', {
-//   preferCanvas: true,
-//   renderer: L.canvas({ padding: 0.5 }),
-//   layers: [base, boundaries['Local authorities']]
-// }).setView([52.561928, -1.464854], 7)
-
-// const baseLayers = {
-//   OpenStreetMap: base
-// }
-
-// const overlay = {
-//   Boundaries: boundaries
-// }
-
-// L.control.groupedLayers(baseLayers, overlay, { hideSingleBase: true, collapsed: false, exclusiveGroups: ['Boundaries'] }).addTo(map)
-
-// var boundaryStyle = {
-//   fillOpacity: 0.5,
-//   weight: 2,
-//   color: 'gray',
-//   fillColor: 'white'
-// }
-
-// var localAuthorityDistricts = []
-// Papa.parse('data/organisation-boundary-referral.csv', {
-//   download: true,
-//   header: true,
-//   step: function (row) {
-//     return localAuthorityDistricts.push(row)
-//   },
-//   complete: function () {
-//     $.ajax({
-//       url: 'https://raw.githubusercontent.com/digital-land/boundaries-collection/master/collection/local-authorities/generalised.geojson'
-//     }).done(function (data) {
-//       var parsed = JSON.parse(data)
-
-//       var englandOnly = parsed.features.filter(function (item) {
-//         return item.properties.lad19cd.startsWith('E')
-//       })
-
-//       parsed.features = englandOnly
-
-//       L.geoJSON(parsed, {
-//         style: boundaryStyle,
-//         onEachFeature: boundaryLabel
-//       }).addTo(boundaries['Local authorities'])
-//     })
-//   }
-// })
-
-// function boundaryLabel (feature, layer) {
-//   var html = localAuthorityDistricts.find(function (item) {
-//     if (item.data['statistical-geography'] && item.data['statistical-geography'].length) {
-//       return item.data['statistical-geography'].toString().toLowerCase() === feature.properties.lad19cd.toString().toLowerCase()
-//     }
-//   })
-
-//   if (html) {
-//     layer.bindPopup(html.data.name, { closeButton: false, offset: L.point(0, -20) })
-//   } else {
-//     layer.setStyle({
-//       fillColor: 'red'
-//     })
-//   }
-
-//   layer.on({
-//     mouseover: function () {
-//       layer.openPopup()
-//       this.setStyle({
-//         fillColor: 'black'
-//       })
-//     },
-//     mouseout: function () {
-//       layer.closePopup()
-//       if (!html) {
-//         this.setStyle({
-//           fillColor: 'red'
-//         })
-//       } else {
-//         this.setStyle({
-//           fillColor: 'white'
-//         })
-//       }
-//     },
-//     click: function () {
-//       return window.location.assign('https://digital-land.github.io/resource/' + html.data.resource + '/#map')
-//     }
-//   })
-
-//   return L.marker(layer.getBounds().getCenter(), {
-//     icon: L.divIcon({
-//       className: 'rounded',
-//       html: html ? html.data['point-count'].toString() : '0',
-//       iconSize: [0, 0]
-//     })
-//   }).addTo(map)
-// }
