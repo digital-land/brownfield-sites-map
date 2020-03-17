21a464397d71044f997bbd044f8733aa2ba72766
Jake Mulley <jakemulley@users.noreply.github.com>
2020-03-17 10:17:25 +0000

    Mimic a 3:2 div

--- docs/boundary-referral.js
+++ docs/boundary-referral.js
@@ -1,8 +1,8 @@
 // Initial map view
 var map = L.map('map', {
-  preferCanvas: true
-  // fullscreenControl: true
-}).setView([52.3555, 1.1743], 7)
+  preferCanvas: true,
+  fullscreenControl: true
+}).setView([52.561928, -1.464854], 7)
 
 // Tile layers
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
